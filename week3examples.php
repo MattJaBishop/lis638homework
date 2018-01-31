8a24091dca78449b5ca15268dc94ee2ceb73778f
MattJaBishop <35987140+MattJaBishop@users.noreply.github.com>
2018-01-31 17:41:19 -0500

    Edits
    More edits

--- week3examples.php
+++ week3examples.php
@@ -39,7 +39,7 @@ switch ($sky) {
 
 # exercise
 
-$speed = 45;
+$speed = 86;
 $currentdate = date("md");
 $birthday = "1128";
 
@@ -57,6 +57,66 @@ if (($speed >= 61) && ($speed <= 80)) {
 		echo "Big ticket";
 }
 
+#while
+
+$counter = 1;
+#while (($speed > 80) && ($counter < 11)) {
+#	echo "BAD BAD TOO FAST TOO FAST<br>";
+#	++$counter;
+#}
+
+#do while
+
+#do {
+# echo "BAD BAD TOO FAST TOO FAST<br>";
+# 	++$counter;
+#}	while (($speed > 80) && ($counter < 11));
+
+#for
+
+for ($counter = 1; $counter < 6; ++$counter) {
+	echo "Right now \$counter is $counter
+	<br>";
+}
+
+
+echo "<table border =\"1\" style='border-collapse: collapse'>";
+	for ($row=1; $row <= 12; $row++) { 
+		echo "<tr> \n";
+		for ($col=1; $col <= 12; $col++) { 
+		   $p = $col * $row;
+		   echo "<td>$p</td> \n";
+		   	}
+	  	    echo "</tr>";
+		}
+		echo "</table>";
+
+		
+		
+		
+echo '<table>';
+
+		
+echo '<table border=1>';
+
+#each row print a tr tag
+
+for ($c = 1;$c <=12;++$c); {
+		echo '<tr>';
+		
+	for ($innercounter = 1;
+	$innercounter <=12;
+	++$innercounter) {
+		echo '<td
+		style="color:red;">';
+		echo $c*$innercounter;
+		echo '</td>';
+}
+		echo '</tr>';
+}
+
+echo '</table>';
+		
 ?>
 </body>
 </html>
