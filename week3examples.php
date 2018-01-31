25ae4a8d16b22d2134f01acc5c3eceaa9c6efb69
MattJaBishop <35987140+MattJaBishop@users.noreply.github.com>
2018-01-31 16:30:41 -0500

    New
    More edits

--- week3examples.php
+++ week3examples.php
@@ -4,7 +4,7 @@
 echo "Hello again, world!";
 
 # Conditionals
-
+# if, else, elseif
 $tempf = 40;
 $sky = "A little cloudy";
 
@@ -15,6 +15,46 @@ if (($tempf >= 32) && ($tempf <= 80)) {
 }
 	else {
 		echo "<p>Uh oh, it's freezing!<p>";
+}
+
+# switch
+
+switch ($sky) {
+	case "A little cloudy":
+		echo ":(";
+		break;
+	case "Sunny":
+		echo ":)";
+		break;
+	case "Windy":
+		echo ";)";
+		break;
+	case "Rainy":
+		echo "Meh";
+		break;
+	default:
+		echo "No idea";
+		break;
+}
+
+# exercise
+
+$speed = 45;
+$currentdate = date("md");
+$birthday = "1128";
+
+if ($birthday==$currentdate) {
+	echo "It's your birthday!";
+	$speed = $speed-5;
+}
+
+if (($speed >= 61) && ($speed <= 80)) {
+	echo "Small ticket";
+} 	elseif ($speed < 60) {
+		echo "No ticket";
+}
+	else {
+		echo "Big ticket";
 }
 
 ?>
