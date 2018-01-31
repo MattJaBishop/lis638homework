a018cc6598f0cb9ab6c6c5d14b035c2eb17c8e6c
MattJaBishop <35987140+MattJaBishop@users.noreply.github.com>
2018-01-31 15:37:05 -0500

    Edit2
    New edit

--- week3examples.php
+++ week3examples.php
@@ -2,6 +2,16 @@
 <body>
 <?php
 echo "Hello again, world!";
+
+# Conditionals
+
+$tempf = 32;
+$sky = "A little cloudy";
+
+if (tempf > 32) {
+	echo "It's not freezing outside!";
+}
+
 ?>
 </body>
 </html>
